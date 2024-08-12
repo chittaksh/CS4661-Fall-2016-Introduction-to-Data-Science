ebc34bb35a5c98353ac050c4a8f55b4f10e90ccb
Chittaksh <khadse.chittaksh@gmail.com>
2024-08-12 09:59:57 -0700

    Update test_results.py

--- Final-Amazon/test_results.py
+++ Final-Amazon/test_results.py
@@ -1,7 +1,4 @@
 import pandas as pd
-
-
-import pandas as pd
 import matplotlib.pyplot as plt
 
 df_train= pd.read_csv('https://raw.githubusercontent.com/vidushidikshit/Machine_Learning/master/train_amazon.csv')
@@ -10,6 +7,5 @@ df_threshold = pd.read_csv('0.3.csv')
 #print(df_threshold)
 df_threshold.plot(x='RESOURCE', y='ACTION', style='ro')
 df_train.plot(x='RESOURCE', y='ACTION', style='o')
-
 
 plt.show()
